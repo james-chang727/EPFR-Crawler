7d8b8bde44a2a831205d3735a3e76c6fdf268b7b
James Chang <jameschang@dotcomintl.com>
2020-09-18 10:04:22 +0800

    master

--- crawler.py
+++ crawler.py
@@ -1,4 +1,18 @@
 import selenium as se
-from selenium.webdriver.chrome import webdriver 
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
-driver = webdriver('C:\Users\jameschang\Desktop\chromedriver.exe')
+PATH = r'C:\Users\jameschang\Desktop\chromedriver.exe'
+driver = webdriver.Chrome(PATH)
+
+driver.get('http://next.epfrglobal.com')
+
+try:
+    element = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.ID, "myDynamicElement"))
+    )
+finally:
+    driver.quit()
+
